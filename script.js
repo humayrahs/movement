f4215e453d239ba3d0bc8bdd7f5aa9ba72c8278f
humayrahs <humayrahsiddique@gmail.com>
2022-12-15 11:07:18 +0000

    player Class made

--- script.js
+++ script.js
@@ -2,9 +2,28 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-//draw player
-ctx.fillStyle = "blue";
-let playerXPos = 10;
-let playerYPos = 10;
-let playerSize = 40;
-ctx.fillRect(playerXPos,playerYPos,playerSize,playerSize);
+//classes
+class Player{
+    constructor(xPos,yPos,xSize,ySize){
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.xSize = xSize;
+        this.ySize = ySize;
+    }
+    drawPlayer(){
+        ctx.fillStyle = "blue";
+        ctx.fillRect(this.xPos,this.yPos,this.xSize,this.ySize);
+    }
+}
+
+class Obstacle{
+
+}
+   
+//create objects
+const pc = new Player(10,10,40,40); //init x/y pos top left
+pc.drawPlayer();
+
+
+
+
